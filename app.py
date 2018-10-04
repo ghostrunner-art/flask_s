db7dc49bbd228ec4d639f3529ef6e2105d84c7a6
jiqingfen110 <313466006@qq.com>
2018-10-05 00:21:32 +0800

    201810501

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask,url_for
 
 app = Flask(__name__)
-
-
-@app.route('/')
+app.config.from_object('setting.Set')
+@app.route('/',endpoint='index') #路由系统，带参数的装饰器，method参数是请求方法，endpoint反向生成URL，其他看源码
 def hello_world():
-    return 'Hello World!'
-
+    return 'hello world!'
 
 if __name__ == '__main__':
     app.run()
---
+++ setting.py
@@ -0,0 +1,2 @@
+class Set():
+    DEBUG = False
