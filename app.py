ff460562bfd41f8c5680d83bb5fd3bde05435e33
jiqingfen110 <313466006@qq.com>
2018-10-16 23:08:22 +0800

    20181016

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-
-from flask import Flask,url_for
+from flask import Flask, request
 
 app = Flask(__name__)
+
+
 # app.config.from_object('setting.Set') # 配置项
 
-@app.route('/',redirect_to='http://www.baidu.com') #路由系统，带参数的装饰器，method参数是请求方法，endpoint反向生成URL，其他看源码
+@app.route('/')  # 路由系统，带参数的装饰器，method参数是请求方法，endpoint反向生成URL，其他看源码
 def hello_world():
-    return 'hello world!'
+
+    s = request.remote_addr
+    return '你当前的ip地址为'+s
+
 
 if __name__ == '__main__':
     app.run()
-    app.__call__
-
-
-
-
-
